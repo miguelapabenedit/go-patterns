4db7a0cd466b72cc3102cae8a68c7fdd15017cd6
miguelapabenedit <miguell.beneditt@gmail.com>
2022-06-09 09:58:02 -0300

    add boundedWorkPooling

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
-	fanOutSemaphore()
+	boundedWorkPooling()
 }
 
 // A Goroutine foundation pattern used by larger patterns like fan out/in.In this patttern a gourtine is
@@ -196,6 +197,37 @@ func fanOutSemaphore() {
 		fmt.Println(d)
 		fmt.Println("parent: recv signal :", children)
 	}
+
+	time.Sleep(time.Second)
+	fmt.Println("--------------------------------------------")
+}
+
+// BoundedWorkPooling is a pattern that uses pool of goroutines to
+// perform a fixed amount of know work
+func boundedWorkPooling() {
+	work := []string{"paper", "paper", "paper", "paper", 2000: "paper"}
+
+	g := runtime.GOMAXPROCS(0)
+	var wg sync.WaitGroup
+	wg.Add(g)
+
+	ch := make(chan string, g)
+
+	for c := 0; c < g; c++ {
+		go func(child int) {
+			defer wg.Done()
+			for wrk := range ch {
+				fmt.Printf("child %d: recvd signal :%s\n", child, wrk)
+			}
+			fmt.Printf("child %d: recvd shutdown signal\n", child)
+		}(c)
+	}
+
+	for _, wrk := range work {
+		ch <- wrk
+	}
+	close(ch)
+	wg.Wait()
 
 	time.Sleep(time.Second)
 	fmt.Println("--------------------------------------------")
